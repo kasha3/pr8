2c064649ae08b87578f9ee02aad2f63919ab17e6
FadAlex <[fadeevsasha3310@mail.ru]>
2024-12-20 18:43:40 +0500

    Реализован класс погоды Forecast. Фадеев

---
+++ pr8_API/pr8_API/Models/Forecast.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pr8_API.Models
+{
+    public class Forecast
+    {
+        public int Icon { get; set; }
+        public string IconPhrase { get; set; }
+        public string ShortPhrase { get; set; }
+        public string LongPhrase { get; set; }
+        public Wind Wind { get; set; }
+        public RelativeHumidity RelativeHumidity { get; set; }
+        public Temperature WetBulbTemperature { get; set; }
+    }
+    public class RelativeHumidity
+    {
+        public int Minimum { get; set; }
+        public int Maximum { get; set; }
+        public int Average { get; set; }
+    }
+}
